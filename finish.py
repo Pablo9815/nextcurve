914bca6457ff5f1a29295db2d6eb6774145a05b7
Pablo Calderón Piedra <79665231+Pablo9815@users.noreply.github.com>
2023-02-27 14:21:01 -0600

    Update finish.py

--- finish.py
+++ finish.py
@@ -6,18 +6,18 @@ import time
 
 
 def main():
+    os.system("sudo pkill start.py")
     GPIO.cleanup()
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(15, GPIO.OUT)
-    os.system("sudo pkill start.py")
     print("Reiniciando")
     os.system("sudo python3 /home/pablonc/Documentos/start.py")
     start_time = time.time()
     while (time.time() - start_time) < 5:
         GPIO.output(15, GPIO.HIGH)
-        time.sleep(0.2)
+        time.sleep(0.1)
         GPIO.output(15, GPIO.LOW)
-        time.sleep(0.2)
+        time.sleep(0.1)
     GPIO.cleanup()
     
 
